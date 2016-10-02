6b4bf72847e9dcdd1667f9b7afd2e559cd4b9cd5
Hanna <hbarring@mines.edu>
2016-10-02 17:38:33 -0600

    Created a python script that takes in a directory of .dat files and creates a directory of ASCII files in the given directory. In progress: plotting all the ASCII files at once.

---
+++ plotMuonFolder.py
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+import os
+import sys
+import argparse
+from subprocess import call
+
+parser = argparse.ArgumentParser(description='Fit all of the data in a given directory')
+
+convertToAscii = 0
+parser.add_argument('-d', action='store', dest='datFiles', help='Directory which contains the .dat files. If selected, this option will load all the files in DATFILES to ASCIIFILES')
+parser.add_argument('-a', action='store', dest='asciiFiles', help='The folder which contains the output asciiFiles')
+
+results = parser.parse_args()
+
+
+def loadToASCII():
+    call(['mkdir -p results.asciiFiles'], shell=True)
+    for dirName, subdirList, fileList in os.walk(results.datFiles):
+        print('Found directory: %s' % dirName)
+        for fname in fileList:
+            print 'From: %s To: %s'%(dirName+'/'+fname, results.asciiFiles+'/'+fname+'.txt')
+            call(['./anamu -i %s -o %s'%(dirName+'/'+fname, results.asciiFiles+'/'+fname+'.txt')], shell=True)
+            
+# ./anamu -o $1 -i $2
+# rootbuild -o muonHistogram muonHistogram.cc $ROOTLIBS
+# ./muonHistogram $1
+
+
+def makeHistograms():
+    for dirName, subdirList, fileList in os.walk(results.asciiFiles):
+        print('Found directory: %s' % dirName)
+        for fname in fileList:
+            # call(['rootbuild -o muonHistogram muonHistogram.cc $ROOTLIBS'], shell=True)
+            # call(['./muonHistogram %s'%(dirName+'/'+fname)], shell=True)
+            # call(['rt muonHistogramPlot.C'], shell=True)
+
+            call(['/bin/bash', '-i', '-c', 'rootbuild -o muonHistogram muonHistogram.cc $ROOTLIBS'])
+            # call(['/bin/bash', '-i', '-c', './muonHistogram %s'%(dirName+'/'+fname)])
+            # call(['/bin/bash', '-i', '-c', 'rt muonHistogramPlot.C'])
+
+if __name__ == "__main__": 
+    if (results.datFiles != None):
+        loadToASCII()
+
+    makeHistograms()
+
+
+# Walk through folder to make plots
