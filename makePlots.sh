976a35af6a8e638936f24118651a51be9b9b9de1
Hanna <hbarring@mines.edu>
2016-10-02 20:18:16 -0600

    made a bash script to convert .dat files to .txt and do the plotting. Plotting still has bugs

---
+++ makePlots.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+outdir=$1
+echo "using outdir $outdir"
+shift
+mkdir -p $outdir
+for fullfile in $*;
+do
+	echo processing $fullfile
+	filename=$(basename "$fullfile")
+	extension="${filename##*.}"
+	filename="${filename%.*}"
+	echo $extension
+	if test "$extension" = "dat" ; then
+		./anamu -i $fullfile -o $outdir/$filename.txt
+		fullfile=$outdir/$filename.txt
+	# elif [$extension == .txt]; then
+	# 	cp $fullfile $outdir/$filename.txt
+	# else
+	# 	echo "unrecognized input type"
+	# 	exit 1
+	fi
+	./muonHistogram $fullfile
+	rt muonHistogramPlot.C
+
+done
