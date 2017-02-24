f742947e13f5ea25040e812e9d28b750335bc54e
Dylan Kern <dyl.kern@gmail.com>
2017-02-23 20:20:29 -0700

    making histogram of fit slopes for angle-energy combo now works

--- DenseRingSimulationsASCII/ReadData2.cc
+++ DenseRingSimulationsASCII/ReadData2.cc
@@ -1,5 +1,3 @@
-
-// ----------------------------------------------------------------------
 // Header files to include
 
 // C++
@@ -27,9 +25,6 @@
 #include "TROOT.h"
 #include "TStyle.h"
 
-// ----------------------------------------------------------------------
-// Functions go here
-
 // This function is really useful for debugging. Simply calling
 // PL()
 // will output the current line of code that the CINT compiler is currently on
@@ -39,176 +34,216 @@
 //----------------------------------------------------------------------
 using namespace std; // So the compiler interprets cout as std::cout
 
+//struct used to hold data read in from file
 struct DataPoint {
-  double energy;
-  double angle;
-  double wcd_tot;
-  double scint_tot;
+	double energy;
+	double angle;
+	double wcd_tot;
+	double scint_tot;
 };
 
+//Function Declarations
 void usage();
-vector<Double_t> doPlotsByAngle(vector<DataPoint>& data, double angle, vector<Double_t> energies);
+vector<DataPoint> ReadFile(ifstream& input_file, string file);
+vector<double> doPlotsByAngle(vector<DataPoint>& data, double angle, vector<double> energies);
 
-// ----------------------------------------------------------------------
-// Globals
+//Global Variables
 
-// ----------------------------------------------------------------------
-
-
-int main(int argc, char **argv) // argc = number arguments, argv = array containing them
-{                           // so for example ./ReadADST $filename1 $filename2
-                            // has argc = 3.
-
+/*
+argc = number arguments, argv = array containing them so for example ./ReadADST $filename1 $filename2
+has argc = 3.
+*/
+int main(int argc, char **argv)
+{
   // Check if the user specified what files to analyse
-  if (argc < 2) {
-    usage();
-    return 1;
-  }
+	if (argc < 2) 
+	{
+		usage();
+		return 1;
+	}
 
   // Makes your plots look "normal". The default background isn't plain white.
   //gROOT->Reset();
-  gROOT->SetStyle("Plain");
+	gROOT->SetStyle("Plain");
   // This makes all plots drawn have X and Y grid lines.
-  gStyle->SetPadGridX(true);
-  gStyle->SetPadGridY(true);
+	gStyle->SetPadGridX(true);
+	gStyle->SetPadGridY(true);
+	//Remove stats box
+	gStyle->SetOptStat(0);
 
-  vector<Double_t> energies = {18.6, 19.0, 19.5, 20.0};
-  // Declare plots here
+	vector<double> energies = {18.6, 19.0, 19.5, 20.0};
+	vector<double> angles = {0, 12, 25, 36, 45, 53};
 
-  TGraph *mip_vem_all = new TGraph();
-  mip_vem_all->SetMarkerStyle(20);
-  mip_vem_all->SetMarkerColor(kRed);
-
-  TGraph *mip_vem_200 = new TGraph();
-  mip_vem_200->SetMarkerStyle(20);
-  mip_vem_200->SetMarkerColor(kGreen);
-
-  TF1 *f_all = new TF1("f_all","pol1",0,3000);
-  TF1 *f_200 = new TF1("f_200","pol1",0,3000);
-  TH2F *slopes = new TH2F("slopes","Slopes",5,0,5,5,0,5);
+	//args: name, title, num_bins_x, min_x, max_x, num_bins_y, min_y, max_y
+	TH2F *slopes = new TH2F("slopes", "Slopes", 15, 18, 21, 30, 0, 60);
+	slopes->GetZaxis()->SetRangeUser(0.75, 1.1);
 
   // -------------------
-
-  ifstream input_file;
-  string line, file;
-
-  int station_id, scin_sat_status, wcd_sat_status;
-  double r_mc;
-  double scint_tot, scin_em, scin_mu;
-  double wcd_tot, wcd_em, wcd_mu;
-
-  double energy, theta;
-
-  int index_all=0, index_200=0;
-
-  vector<DataPoint> data;
+	vector<DataPoint> data;
+	
   // ----------------------------------------------------------------------
   // Loop over files
   // read the file as the second to last argument
-  for (int iFile = 1; iFile <= argc - 1; iFile++) {
+	for (int iFile = 1; iFile <= argc - 1; iFile++) 
+	{
+		ifstream input_file;
+		string file;
 
-    input_file.open(argv[iFile]);
+		input_file.open(argv[iFile]);
+		file = argv[iFile];
 
-    file = argv[iFile];
-
-    // Fetching energy (from lgenergyXX.XX)
-    string::size_type e = file.find('y');
-    string::size_type ee = file.find('/');
-    stringstream(file.substr(e+1,ee-e-1))>>energy;
-
-    // Fetching theta (from thetaXX)
-    string::size_type t = file.find('a');
-    string::size_type tt = file.find('/');
-    stringstream(file.substr(t+1,tt-t-1))>>theta;
-
-    cout << "reading file: " << file << " Log(E/eV)= " << energy << " Zenith angle (Deg)= " << theta << endl;
-
-    // First line is made of headers - ignoring  
-    getline(input_file,line);
-
-    while (input_file) 
-    {
-      input_file >> station_id >> r_mc
-      >> scint_tot >> scin_em >> scin_mu 
-      >> wcd_tot >> wcd_em >> wcd_mu
-      >> scin_sat_status >> wcd_sat_status;
-      
-      DataPoint newPoint = {energy, theta, wcd_tot, scint_tot};
-      data.push_back(newPoint);
-
-      input_file.close();
-
-    }
-  }
+		vector<DataPoint> newData = ReadFile(input_file, file);
+		input_file.close();
+		data.insert(data.end(), newData.begin(), newData.end());
+	}
 
   // ----------------------------------------------------------------------
-  // If you want to draw any plots do the screen, you first need to declare a TApplication,
-  // the .Run() it before the end of your main function.
+  // If you want to draw any plots to the screen, you first need to declare a TApplication,
+  // then call .Run() before the end of your main function.
 
   // A TApplication is required for drawing
   // Once a TApplication is declared it absorbs the argc and argv parameters, so if you want to
   // do anything with them (like in this code), declare your TApplication afterwards. Otherwise it
   // can be initialised right at the start of main().
-  
-  TApplication theApp("app", &argc, argv);  
-  printf("Complete.\n");
 
-  gStyle->SetPalette(1);
-  
-  vector<Double_t> fit_slopes = doPlotsByAngle(data, 0., energies);
-  TCanvas *c = new TCanvas();
-  for (int i = 0; i < fit_slopes.size(); i ++)
-  {
-    slopes->Fill(energies[i], 0.0, fit_slopes[i]);
-  }
+	TApplication theApp("app", &argc, argv);  
+	printf("Complete.\n");
 
-  slopes->Draw("colz");
+	gStyle->SetPalette(1);
 
-  c->Update();
+	TCanvas *c = new TCanvas();
 
-  theApp.Run();
+	//Go through each angle and make a graph for each energy
+	for (int i = 0; i < angles.size(); i++)
+	{
+		vector<double> fit_slopes = doPlotsByAngle(data, angles[i], energies);
+		for (int j = 0; j < fit_slopes.size(); j++)
+		{
+			cout << fit_slopes[j] << endl;
+			slopes->Fill(energies[j], angles[i], fit_slopes[j]);
+		}
+	}
 
-  return 0;
+	slopes->Draw("colz");
+
+	c->Update();
+
+	theApp.Run();
+
+	return 0;
 
 }
 
+//DO THIS LATER
 void usage()
 {
-  printf("Usage:\n"
-  "Describe here \n");
+	printf("Usage:\n"
+		"Describe here \n");
 }
 
-vector<Double_t> doPlotsByAngle(vector<DataPoint>& data, double angle, vector<Double_t> energies)
+/*
+Reads in a file that has already been opened
+params
+	ifstream input_file file that has been opened to be read
+	string file name of file being read
+*/
+vector<DataPoint> ReadFile(ifstream& input_file, string file)
 {
-  vector<TGraph*> graphs;
-  for(int i = 0; i < energies.size(); i++)
-  {
-    graphs.push_back(new TGraph());
-  }
+	vector<DataPoint> data;
+	int station_id, scin_sat_status, wcd_sat_status;
+	double r_mc;
+	double scint_tot, scin_em, scin_mu;
+	double wcd_tot, wcd_em, wcd_mu;
 
-  int index186=0, index190=0, index195=0, index200=0;
-  for (DataPoint d : data)
-  {
-    if(d.angle == angle)
-    {
+	double energy, theta;
 
-      if (d.energy == energies[0])
-        graphs[0]->SetPoint(index186, d.wcd_tot, d.scint_tot);
-      else if (d.energy == energies[1])
-        graphs[1]->SetPoint(index190, d.wcd_tot, d.scint_tot);
-      else if (d.energy == energies[2])
-        graphs[2]->SetPoint(index195, d.wcd_tot, d.scint_tot);
-      else if (d.energy == energies[3])
-        graphs[3]->SetPoint(index200, d.wcd_tot, d.scint_tot);
-    }
-  }
-  TF1 *fit = new TF1("fit","pol1",0,3000);
-  vector<Double_t> fit_slopes;
-  for (TGraph* graph : graphs)
-  {
-    graph->Draw();
-    graph->Fit("fit");
-    fit_slopes.push_back(fit->GetParameter(1));
-  }
-  return fit_slopes;
+	string line;
+
+	  // Fetching energy (from lgenergyXX.XX)
+	string::size_type e = file.find('y');
+	string::size_type ee = file.find('/');
+	stringstream(file.substr(e+1,ee-e-1))>>energy;
+
+	// Fetching theta (from thetaXX)
+	string::size_type t = file.find('a');
+	string::size_type tt = file.find('/');
+	stringstream(file.substr(t+1,tt-t-1))>>theta;
+
+	//cout << "reading file: " << file << " Log(E/eV)= " << energy << " Zenith angle (Deg)= " << theta << endl;
+
+	// First line is made of headers - ignoring  
+	getline(input_file,line);
+
+	while (input_file) 
+	{
+		input_file >> station_id >> r_mc
+		>> scint_tot >> scin_em >> scin_mu 
+		>> wcd_tot >> wcd_em >> wcd_mu
+		>> scin_sat_status >> wcd_sat_status;
+
+		DataPoint newPoint = {energy, theta, wcd_tot, scint_tot};
+		data.push_back(newPoint);
+	}
+	return data;
+}
+
+/*
+Plots and fits data filtered by angle and energy
+params
+	vector<DataPoint>& data All data to be filtered through
+	double angle the angle to filter by
+	vector<double> energies the energies to filter by
+*/
+vector<double> doPlotsByAngle(vector<DataPoint>& data, double angle, vector<double> energies)
+{
+	vector<TGraph*> graphs;
+	//If we want a graph with all points
+  	//graphs.push_back(new TGraph()); //All energies
+	for(int i = 0; i < energies.size(); i++)
+	{
+		graphs.push_back(new TGraph());
+	}
+
+	int indexes[4] = {0, 0, 0, 0};
+
+	//Filters into several graphs
+	for (DataPoint d : data)
+	{
+		if(d.angle == angle)
+		{
+			//graphs[0]->SetPointindexes[0], d.wcd_tot, d.scint_tot);
+			//indexes[0]++;
+			//This could be simplified with an int set in the if/else, then add to graph after
+			if (d.energy == energies[0])
+			{
+				graphs[0]->SetPoint(indexes[0], d.wcd_tot, d.scint_tot);
+				indexes[0]++;
+			}
+			else if (d.energy == energies[1])
+			{
+				graphs[1]->SetPoint(indexes[1], d.wcd_tot, d.scint_tot);
+				indexes[1]++;
+			}
+			else if (d.energy == energies[2])
+			{
+				graphs[2]->SetPoint(indexes[2], d.wcd_tot, d.scint_tot);
+				indexes[2]++;
+			}
+			else if (d.energy == energies[3])
+			{
+				graphs[3]->SetPoint(indexes[3], d.wcd_tot, d.scint_tot);
+				indexes[3]++;
+			}
+		}
+	}
+	//Fit each graph and get fit result
+	TF1 *fit = new TF1("fit","pol1",0,3000);
+	vector<double> fit_slopes;
+	for (TGraph* graph : graphs)
+	{
+		graph->Draw();
+		graph->Fit("fit", "q");
+		fit_slopes.push_back(fit->GetParameter(1));
+	}
+	return fit_slopes;
 }
